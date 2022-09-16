d9a6f7d1b64dca4e114efa237a6f9d8ae4a9537b
maodesigns <mao.devv@gmail.com>
2022-09-16 21:30:47 +0100

    Updated!

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,10 +4,8 @@ import { AiOutlinePlus } from "react-icons/ai";
 
 import { ToastContainer, toast } from "react-toastify";
 
-
 // Import Components
 import List from "./List";
-
 
 const toastOptions = {
   theme: "colored",
@@ -29,13 +27,15 @@ const useLocalStorage = () => {
 
 function App() {
   const [show, setShow] = useState(false);
-  
+
   const [isEditing, setIsEditing] = useState(false);
   const [editedID, setEditedID] = useState("");
 
   const inputEl = useRef(null);
   const [todoList, setTodoList] = useState(useLocalStorage());
   const [todo, setTodo] = useState({ id: "", item: "" });
+
+  // const [state, dispatch] = useReducer()
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -66,9 +66,10 @@ function App() {
 
   const handleCompleted = (e) => {
     const curItem = e.target;
+    console.log();
     if (curItem.classList.contains("completed")) return;
-    else if (curItem.classList.contains("grocery-item")) {
-      const editBtn = curItem.children[1].children[0];
+    else if (curItem.classList.contains("title")) {
+      const editBtn = curItem.nextSibling.children[0];
       curItem.classList.add("completed");
       editBtn.remove();
       toast.success("Good Job! Todo completed.", { ...toastOptions });
@@ -129,18 +130,21 @@ function App() {
           editItem={editItem}
         />
         {todoList.length > 0 && (
-          <button
-            className="clear-btn"
-            onClick={() => {
-              toast.success("Successfully cleared all Items.", {
-                ...toastOptions,
-              });
+          <>
+            <p>Once a Task is completed, click on the text.</p>
+            <button
+              className="clear-btn"
+              onClick={() => {
+                toast.success("Successfully cleared all Items.", {
+                  ...toastOptions,
+                });
 
-              setTodoList([]);
-            }}
-          >
-            Clear items
-          </button>
+                setTodoList([]);
+              }}
+            >
+              Clear items
+            </button>
+          </>
         )}
 
         <button
